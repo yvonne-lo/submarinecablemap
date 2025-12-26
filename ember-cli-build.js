95e374a70ba4aaba97d07f17d31f0567f49cc009
yvonne-lo <yvonnelo@cht.com.tw>
2025-12-26 15:07:44 +0800

    Update ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    babel: {
+      plugins: [
+        require.resolve('@babel/plugin-proposal-optional-chaining')
+      ]
+    }
     // Add options here
     // fingerprint: {
     //   enabled: true,
