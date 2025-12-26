84bf6141cb88978f6379b413ff2bcbbbbe896564
yvonne-lo <yvonnelo@cht.com.tw>
2025-12-26 11:25:51 +0800

    Update map-container.js

--- app/components/map-container.js
+++ app/components/map-container.js
@@ -1,3 +1,4 @@
+
 /* global SubmarineCable */
 import Component from '@ember/component';
 import { getOwner } from '@ember/application';
@@ -5,12 +6,52 @@ import $ from 'jquery';
 
 export default Component.extend({
   classNames: ['map-container'],
-  didInsertElement(){
+
+  didInsertElement() {
     this._super(...arguments);
-    let application = getOwner(this).application;
+    const application = getOwner(this).application;
     $('body').find('.ember-view').first().addClass('map-container');
-    if(!application.map && application.mapConfig){
-      application.set('map', new SubmarineCable.Map('map', application.mapConfig));
+
+    if (!application.selectedCableIds) {
+      application.set('selectedCableIds', []);
+    }
+
+    if (!application.map && application.mapConfig) {
+      const mapConfig = Object.assign({}, application.mapConfig, {
+        onCableClick: (feature, nativeEvt) => {
+          const id =
+            feature?.properties?.id ||
+            feature?.id ||
+            feature?.properties?.slug;
+          if (!id) return;
+
+          const isMulti = nativeEvt?.metaKey || nativeEvt?.ctrlKey;
+          const set = new Set(application.selectedCableIds);
+
+          if (set.has(id)) {
+            set.delete(id);           
+          } else {
+            if (!isMulti) set.clear();
+            set.add(id);              
+          }
+          application.set('selectedCableIds', Array.from(set));
+
+          if (application.map?.refreshStyles) {
+            application.map.refreshStyles(application.selectedCableIds);
+          }
+        },
+
+        onMapClickBlank: () => {
+          application.set('selectedCableIds', []);
+          if (application.map?.refreshStyles) {
+            application.map.refreshStyles([]);
+          }
+        },
+
+        selectedIds: () => application.selectedCableIds
+      });
+
+      application.set('map', new SubmarineCable.Map('map', mapConfig));
     }
   }
 });
